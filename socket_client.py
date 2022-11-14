bca3ce7b912530115b9c832ce26daa363ad312e6
jademnchx <jade.mnchx@gmail.com>
2022-11-14 10:21:35 +0100

    master

--- socket_client.py
+++ socket_client.py
@@ -8,7 +8,11 @@ if __name__ == '__main__':
     client_socket = socket.socket()
     client_socket.connect(('127.0.0.1', 10000))
     print ("Connected to server")
-    client_socket.send('start'.encode())
+    t=client_socket.send(input().encode())
+    while t != 'stop':
+        t
+    else :
+        client_socket.close()
+        print ("Connection closed")
     data = client_socket.recv(1024).decode()
-    print ("Received: " + data)
-    client_socket.close()
+    print ("Received from server: ", data)
--- socket_server.py
+++ socket_server.py
@@ -11,6 +11,10 @@ if __name__ == '__main__':
     server_socket.listen(1)
     conn, address = server_socket.accept()
     data = conn.recv(1024).decode()
-    conn.send('stop'.encode())
-    print ("Received: " + data)
-    conn.close()
+    print ("Received from client: " + data)
+    while conn.send(input().encode()) != 'stop':
+        l = conn.send(input().encode())
+        print ("Sent to client: ", l)
+    else:
+        conn.close()
+        print ("Connection closed")
