f4d7c036d1ee88f7200e20c468123e69999512a2
jademnchx <jade.mnchx@gmail.com>
2022-11-15 10:56:22 +0100

    jade

--- client1.py
+++ client1.py
@@ -14,7 +14,5 @@ if __name__ == '__main__':
         client_socket.send(msg.encode())
         data = client_socket.recv(1024).decode()
         print ("Received from server: ", data)
-    
-    
     client_socket.close()
     print ("Connection closed")
